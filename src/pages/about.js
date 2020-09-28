f52c76333bed5b5882724deaa51a513d1479db43
Carl Mueller <cmuell89@gmail.com>
2020-09-28 14:33:48 -0600

    More content

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,11 @@ import Img from "gatsby-image"
 const ContentContainer = styled.div`
   max-width: 40em;
   padding: 0 1em;
+
+`;
+
+const FigCaption = styled.figcaption`
+  font-size: 14px;
 `;
 
 export default function About({ data, location }) {
@@ -20,34 +25,39 @@ export default function About({ data, location }) {
         <section>
           <p>Currently, I am senior member of <a href="https://www.cairo-lab.com/" title="CAIRO Lab">CAIRO lab</a> at CU Boulder, advised by Professor Bradley Hayes. I also helped co-found a technology startup called <a href="https://lightninginabot.com" title="Lightning in a Bot">Lightning in a Bot</a>, which developed the Shoppy Bot product, a novel reporting software for ecommerce businesses.</p>
           <figure>
-            <Img fluid={data.imageOne.childImageSharp.fluid} />
-            <figcaption>Venice Beach with Missy circa 2015.</figcaption>
+            <Img fluid={data.VeniceWithMissy.childImageSharp.fluid} />
+            <FigCaption>Venice Beach with Missy circa 2015.</FigCaption>
           </figure>
         </section>
         <h3>A Brief History Of Time</h3>
         <section>
           <p>In the summer of 2007, I left my home state of Massachusetts and moved to California to attend the University of California - Santa Barbara.
-            After graduating in 2011 with a B.S. in Biopsychology, I was fortunate to be hired by QualTek Molecular Laboratories where I worked for three years as a research scientist. During my time in CA, I developed a keen interest in neuroscience and an obsession with computing technology and theory. I also co-founded of Lightning in a Bot, Inc with a good friend. We got our start as a natural language UI-driven business analytics company and expanded to custom software development. Running a company was one of the most rewarding experiences of my life, but I knew I wanted to learn more about computer science and about technologies designed to work with human beings. I made my way to Boulder, Colorado and started my PhD in Robotics / Human-Robot Interaction. </p>
+            After graduating in 2011 with a B.S. in Biopsychology, I was fortunate to be hired by QualTek Molecular Laboratories where I worked for three years as a research scientist. During my time in CA, I developed a keen interest in neuroscience and an obsession with computing technology and theory. I also co-founded of Lightning in a Bot, Inc with a good friend. We got our start as a natural language UI-driven business analytics company and expanded to custom software development. Running a company was one of the most rewarding experiences of my life, but I knew I wanted to learn more about computer science and about technologies designed to work with human beings. I made my way to Boulder, Colorado and started my PhD in Robotics / Human-Robot Interaction.
+          </p>
+          <figure>
+            <Img fluid={data.FrostedFlatirons.childImageSharp.fluid} />
+            <FigCaption>Early morning view of the Flatirons after a health snowfall.</FigCaption>
+          </figure>
           <br></br>
         </section>
         <h3>When I'm not yelling at robots...</h3>
         <section>
           <p>I love to mountain bike. While I enjoy road cycling, gravel, and cyclocross, mountain biking is absolutely the most fun thing in the world.</p>
           <figure>
-            <Img fluid={data.imageTwo.childImageSharp.fluid} />
-            <figcaption>CU Boulder Short Track Racing.</figcaption>
+            <Img fluid={data.CUShortTrack.childImageSharp.fluid} />
+            <FigCaption>CU Boulder Short Track Racing.</FigCaption>
           </figure>
           <br></br>
           <p>I recently picked up snowboarding a couple of years ago, but it gets more and more fun each season. The views in the CO mountains are unreal.</p>
           <figure>
-            <Img fluid={data.imageThree.childImageSharp.fluid} />
-            <figcaption>The summit of Arapahoe Basin, one of my favorite places on earth.</figcaption>
+            <Img fluid={data.Abasin.childImageSharp.fluid} />
+            <FigCaption>The summit of Arapahoe Basin, one of my favorite places on earth.</FigCaption>
           </figure>
           <br></br>
           <p>I also enjoy cooking, playing bass guitar, watching movies, and Boston sports.</p>
           <figure>
-            <Img fluid={data.imageFour.childImageSharp.fluid} />
-            <figcaption>Red Sox Dodgers circa 2013. The time flies!</figcaption>
+            <Img fluid={data.RedSoxDodgers.childImageSharp.fluid} />
+            <FigCaption>Red Sox Dodgers circa 2013. The time flies!</FigCaption>
           </figure>
         </section>
       </ContentContainer>
@@ -57,7 +67,7 @@ export default function About({ data, location }) {
 
 export const query = graphql`
   query {
-    imageOne: file(relativePath: { eq: "VeniceWithMissy.jpg"}) {
+    VeniceWithMissy: file(relativePath: { eq: "VeniceWithMissy.jpg"}) {
       childImageSharp {
         fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
@@ -65,7 +75,7 @@ export const query = graphql`
         }
       }
     }
-    imageTwo: file(relativePath: { eq:  "CUShortTrack.jpg" }) {
+    FrostedFlatirons: file(relativePath: { eq:  "FrostedFlatirons.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
@@ -73,7 +83,7 @@ export const query = graphql`
         }
       }
     }
-    imageThree: file(relativePath: { eq: "Abasin.jpg" }) {
+    CUShortTrack: file(relativePath: { eq:  "CUShortTrack.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
@@ -81,7 +91,15 @@ export const query = graphql`
         }
       }
     }
-    imageFour: file(relativePath: { eq: "RedSoxDodgers.jpg" }) {
+    Abasin: file(relativePath: { eq: "Abasin.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluidLimitPresentationSize
+        }
+      }
+    }
+    RedSoxDodgers: file(relativePath: { eq: "RedSoxDodgers.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 600) {
           ...GatsbyImageSharpFluid
