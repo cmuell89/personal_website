cb97fa8ed971d328ac18a9b5011293ee7244fc0f
Carl Mueller <cmuell89@gmail.com>
2020-09-28 12:33:33 -0600

    Updating config for links.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 module.exports = {
   title: 'Carl Mueller',
   url: 'https://www.carl-mueller.com',
+  githubUrl: 'https://github.com/cmuell89',
+  linkedInUrl: 'https://www.linkedin.com/in/carl-mueller-794b9a26/',
+  twitterUrl: 'https://twitter.com/cmuell89',
   formActionURL: 'https://formspree.io/xzbkbnod',
   formSubmittedURL: 'https://dylan-la.com/message-submitted'
 };
